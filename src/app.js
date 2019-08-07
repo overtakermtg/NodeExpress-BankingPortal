33a66b30f1e36483ddf6ca58e4cc5d98832a6a09
overtakermtg <angel.valenzuelafarfan@scotiabank.com>
2019-08-06 21:36:16 -0400

    second module

--- src/app.js
+++ src/app.js
@@ -8,6 +8,43 @@ app.set('view engine', 'ejs');
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', (req, res) => res.render('index', { title: 'Index' }));
 
-app.listen(3000, () => console.log('PS project running on port 3000'))
+
+const accountData = fs.readFileSync(
+    path.join(__dirname, 'json', 'accounts.json'), 'utf8'
+);
+const accounts = JSON.parse(accountData);
+
+const userData = fs.readFileSync(
+    path.join(__dirname, 'json', 'users.json'), 'utf8'
+);
+const user = JSON.parse(userData);
+
+
+app.get('/savings', (req, res) => { 
+    res.render('account', { account : accounts.savings });
+});
+
+app.get('/savings', (req, res) => { 
+    res.render('account', { account : accounts.savings });
+});
+app.get('/checking', (req, res) => { 
+    res.render('account', { account : accounts.checking });
+});
+app.get('/credit', (req, res) => { 
+    res.render('account', { account : accounts.credit });
+});
+
+app.get('/payment', (req, res) => { 
+    res.render('payment', { account : accounts.credit });
+});
+
+app.get('/profile', (req, res) => { 
+    res.render('profile', { user: user[0], title : 'Profile' });
+});
+
+
+// ese account del final le da acceso al sitio llamado a ese objeto
+app.get('/', (req, res) => res.render('index', { title: 'Account Summary', accounts }));
+
+app.listen(3000, () => console.log('PS project running on port 3000'));
